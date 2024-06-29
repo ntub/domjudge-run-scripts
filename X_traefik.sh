1967e5d15c3eb72f69ba6a2654f54dce17a8b6e8
Arthur Chang <arthurc0102@gmail.com>
2024-06-29 18:21:53 +0800

    feat: add traefik letsEncrypt config

--- X_traefik.sh
+++ X_traefik.sh
@@ -10,6 +10,7 @@ docker run -d \
     -v ./traefik/traefik.yml:/etc/traefik/traefik.yml:ro \
     -v ./traefik/config/domserver-$TRAEFIK_DOMSERVER_MODE.yml:/etc/traefik/config/domserver.yml:ro \
     -v ./traefik/ssl:/etc/traefik/ssl:ro \
+    -v ./traefik/acme:/etc/traefik/acme \
     -p 80:80 \
     -p 443:443 \
     -e DOMSERVER_DOMAIN=$DOMSERVER_DOMAIN \
